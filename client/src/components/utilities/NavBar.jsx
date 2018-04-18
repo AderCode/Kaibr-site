bc5fe0ab92ea70800ecdaf686094467df3c8c70a
Matthew Aderhold <matthewaderhold@gmail.com>
2018-04-17 20:10:11 -0500

    changed 'class' to 'className'

--- client/src/components/utilities/NavBar.jsx
+++ client/src/components/utilities/NavBar.jsx
@@ -3,31 +3,31 @@ import React, { Component } from "react";
 export default class NavBar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-dark deep-purple lighten-1 py-0 px-3 m-0 fixed-top">
-                <a class="navbar-brand" href="/">
+            <nav className="navbar navbar-expand-lg navbar-dark deep-purple lighten-1 py-0 px-3 m-0 fixed-top">
+                <a className="navbar-brand" href="/">
                     <img src="https://s3.amazonaws.com/kaibr/logo/TEMP_LOGO.png" alt="K" height="35" /> Kaibr
                 </a>
 
                 {/* <!-- Collapse button --> */}
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
                     aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
                 {/* <!-- Collapsible content --> */}
-                <div class="collapse navbar-collapse" id="basicExampleNav">
+                <div className="collapse navbar-collapse" id="basicExampleNav">
 
                     {/* <!-- Links --> */}
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">Home
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/about">About</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">About</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/contact">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/contact">Contact</a>
                         </li>
                     </ul>
                     {/* <!-- Links --> */}
